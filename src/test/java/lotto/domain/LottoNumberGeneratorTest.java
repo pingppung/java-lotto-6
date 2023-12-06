3bb316c41954c3d1488b8394e1d65e7475c71a0d
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-07 02:32:41 +0900

    test(LottoNumberGeneratorTest): 로또 생성 시 번호 범위가 1~45 내에 속하는지 확인하는 테스트 추가

--- src/test/java/lotto/domain/LottoNumberGeneratorTest.java
+++ src/test/java/lotto/domain/LottoNumberGeneratorTest.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import static lotto.constants.LottoConstants.MAX_NUMBER;
+import static lotto.constants.LottoConstants.MIN_NUMBER;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
@@ -8,22 +10,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoNumberGeneratorTest {
-
-    private LottoNumberGenerator numberGenerator;
     private List<Integer> generatedNumbers;
 
     @BeforeEach
     void setUp() {
-        numberGenerator = new LottoNumberGenerator();
+        LottoNumberGenerator numberGenerator = new LottoNumberGenerator();
         generatedNumbers = numberGenerator.generate();
     }
 
     @DisplayName("생성된_로또_번호_개수_확인")
     @Test
     void generateLottoNumbersCount() {
-
         assertThat(generatedNumbers).hasSize(6);
     }
 
-
+    @DisplayName("생성된_로또_번호_범위_확인")
+    @Test
+    void generateLottoNumbersRange() {
+        assertThat(generatedNumbers).allMatch(
+                number -> number >= MIN_NUMBER.getValue() && number <= MAX_NUMBER.getValue());
+    }
 }
