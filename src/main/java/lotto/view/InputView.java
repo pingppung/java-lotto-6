b06e13ad961ab5f7a369923ecaff0faa21be84a9
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-07 02:52:14 +0900

    feat(InputView): 로또 당첨 번호를 입력하는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -4,9 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
+    private static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
 
     public String getPurchaseAmount() {
         return inputWithPrompt(PURCHASE_AMOUNT_PROMPT);
+    }
+
+    public String getWinningNumber() {
+        return inputWithPrompt(WINNING_NUMBER_PROMPT);
     }
 
     private String inputWithPrompt(String prompt) {
