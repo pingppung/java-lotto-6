7add84c551de171b7babaa35a6b4d7e73f1cd14e
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-07 01:43:56 +0900

    test(LottoPurchaseTest): 입력 금액에 따른 구매 가능한 로또 개수 계산 테스트

---
+++ src/test/java/lotto/domain/LottoPurchaseTest.java
@@ -0,0 +1,18 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoPurchaseTest {
+
+    @DisplayName("구매할_수_있는_로또_개수_계산")
+    @Test
+    void calculateLottoCount() {
+        int testMoney = 100000;
+        LottoPurchase lottoPurchase = new LottoPurchase(String.valueOf(testMoney));
+        int calculateCount = lottoPurchase.calculateLottoCount(testMoney);
+        assertThat(testMoney / 1000).isEqualTo(calculateCount);
+    }
+}
