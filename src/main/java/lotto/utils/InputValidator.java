bfe8189687ee11e58c21a1789d656c3f77f5a908
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-07 15:08:16 +0900

    feat(InputValidator): 당첨 번호 쉼표를 기준으로 분리 후, 번호들이 다 숫자인지 판별하는 기능 구현

--- src/main/java/lotto/utils/InputValidator.java
+++ src/main/java/lotto/utils/InputValidator.java
@@ -1,11 +1,22 @@
 package lotto.utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputValidator {
-    public static int isNonNumeric(String input) {
+    public static int validateNonNumeric(String input) {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("[ERROR] 숫자만 입력 가능합니다");
         }
+    }
+
+    public static List<Integer> validateNonNumericWinningNumbers(List<String> inputs) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String input : inputs) {
+            numbers.add(validateNonNumeric(input));
+        }
+        return numbers;
     }
 }
