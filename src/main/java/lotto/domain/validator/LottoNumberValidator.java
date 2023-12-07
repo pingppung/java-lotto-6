177efc7b5ec1f33757698c9f9411a408a76e685b
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-08 02:43:34 +0900

    feat(LottoNumberValidator): 입력한 당첨 번호끼리 중복되는 숫자가 존재하는 경우 예외처리 구현

--- src/main/java/lotto/domain/validator/LottoNumberValidator.java
+++ src/main/java/lotto/domain/validator/LottoNumberValidator.java
@@ -4,24 +4,34 @@ import static lotto.constants.LottoConstants.MAX_NUMBER;
 import static lotto.constants.LottoConstants.MIN_NUMBER;
 import static lotto.constants.LottoConstants.NUMBERS_PER_GAME;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LottoNumberValidator {
-    public void validateNumberInRange(int number) {
+
+    public static void validateNumberInRange(int number) {
         if (number < MIN_NUMBER.getValue() || number > MAX_NUMBER.getValue()) {
             throw new IllegalArgumentException("[ERROR] 1~45 숫자만 가능합니다.");
         }
     }
 
-    public void validateInRangeWinningNumbers(List<Integer> numbers) {
+    public static void validateNumbersInRange(List<Integer> numbers) {
         for (int number : numbers) {
             validateNumberInRange(number);
         }
     }
 
-    public void validateNumbersCount(List<Integer> numbers) {
+    public static void validateNumbersCount(List<Integer> numbers) {
         if (numbers.size() != NUMBERS_PER_GAME.getValue()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개이여야 합니다.");
+        }
+    }
+
+    public static void validateDuplicateNumbers(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 존재합니다.");
         }
     }
 }
