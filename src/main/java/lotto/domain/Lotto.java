5d4287be84674825ca718de20361612fb7391323
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-08 02:47:45 +0900

    feat(Lotto): 로또 번호 오름차순으로 정렬되도록 Lotto에서 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,5 +1,7 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import lotto.domain.validator.LottoNumberValidator;
 
@@ -9,12 +11,18 @@ public class Lotto {
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = sortNumbers(numbers);
     }
 
     private void validate(List<Integer> numbers) {
         LottoNumberValidator.validateNumbersInRange(numbers);
         LottoNumberValidator.validateNumbersCount(numbers);
         LottoNumberValidator.validateDuplicateNumbers(numbers);
+    }
+
+    private List<Integer> sortNumbers(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
+        Collections.sort(sortedNumbers);
+        return sortedNumbers;
     }
 }
--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -5,14 +5,11 @@ import static lotto.constants.LottoConstants.MIN_NUMBER;
 import static lotto.constants.LottoConstants.NUMBERS_PER_GAME;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
     public List<Integer> generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER.getValue(), MAX_NUMBER.getValue(),
+        return Randoms.pickUniqueNumbersInRange(MIN_NUMBER.getValue(), MAX_NUMBER.getValue(),
                 NUMBERS_PER_GAME.getValue());
-        Collections.sort(numbers);
-        return numbers;
     }
 }
