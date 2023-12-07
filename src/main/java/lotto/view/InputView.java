e0fd04f2817eea59ba319e802dd9b551b30f4600
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-07 15:14:23 +0900

    refactor(InputView): View에서 간단하게 숫자인지 확인하는 유효성 검사를 수행

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,22 +1,29 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.utils.InputValidator;
+import lotto.utils.Parser;
 
 public class InputView {
     private static final String PURCHASE_AMOUNT_PROMPT = "구입금액을 입력해 주세요.";
     private static final String WINNING_NUMBER_PROMPT = "당첨 번호를 입력해 주세요.";
     private static final String BONUS_NUMBER_PROMPT = "보너스 번호를 입력해 주세요.";
 
-    public String getPurchaseAmount() {
-        return inputWithPrompt(PURCHASE_AMOUNT_PROMPT);
+    public int getPurchaseAmount() {
+        String input = inputWithPrompt(PURCHASE_AMOUNT_PROMPT);
+        return InputValidator.validateNonNumeric(input);
     }
 
-    public String getWinningNumber() {
-        return inputWithPrompt(WINNING_NUMBER_PROMPT);
+    public List<Integer> getWinningNumber() {
+        String input = inputWithPrompt(WINNING_NUMBER_PROMPT);
+        List<String> parserInput = Parser.parseWinnginNumbers(input);
+        return InputValidator.validateNonNumericWinningNumbers(parserInput);
     }
 
-    public String getBonusNumber() {
-        return inputWithPrompt(BONUS_NUMBER_PROMPT);
+    public int getBonusNumber() {
+        String input = inputWithPrompt(BONUS_NUMBER_PROMPT);
+        return InputValidator.validateNonNumeric(input);
     }
 
     private String inputWithPrompt(String prompt) {
