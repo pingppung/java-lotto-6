d8164baa7173cc6dfd95d404b7bf2d9b78d3b5a2
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-09 02:40:38 +0900

    feat(LottoPurchaseService): 로또 구매에 관련된 기능을 처리하는 서비스 클래스 구현

---
+++ src/main/java/lotto/services/LottoPurchaseService.java
@@ -0,0 +1,25 @@
+package lotto.services;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoCountCalculator;
+import lotto.domain.LottoFactory;
+
+public class LottoPurchaseService {
+    private final LottoFactory lottoFactory;
+    private final LottoCountCalculator lottoCountCalculator;
+
+    public LottoPurchaseService(int purchaseAmount) {
+        this.lottoFactory = new LottoFactory();
+        this.lottoCountCalculator = new LottoCountCalculator(purchaseAmount);
+    }
+
+    public List<Lotto> purchaseLottos(int purchaseAmount) {
+        int numberOfLottos = calculateLottoCount();
+        return lottoFactory.generateLottos(numberOfLottos);
+    }
+
+    private int calculateLottoCount() {
+        return lottoCountCalculator.calculateLottoCount();
+    }
+}
