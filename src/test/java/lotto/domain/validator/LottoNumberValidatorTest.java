e10dd6628390cc8aa72298993f9b73be385425de
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-08 15:42:26 +0900

    test(LottoNumberValidatorTest): 로또 번호가 6개인지 확인하는 유효성 검사 테스트

--- src/test/java/lotto/domain/validator/LottoNumberValidatorTest.java
+++ src/test/java/lotto/domain/validator/LottoNumberValidatorTest.java
@@ -3,6 +3,7 @@ package lotto.domain.validator;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -22,5 +23,21 @@ public class LottoNumberValidatorTest {
         assertThatThrownBy(() -> LottoNumberValidator.validateNumberInRange(invalidNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 1~45 숫자만 가능합니다.");
+    }
+
+    @DisplayName("6개의_숫자_리스트_테스트")
+    @Test
+    void validateNumbersCount() {
+        List<Integer> valideNumbers = List.of(1, 2, 3, 4, 5, 6);
+        assertDoesNotThrow(() -> LottoNumberValidator.validateNumbersCount(valideNumbers));
+    }
+
+    @DisplayName("6개의_숫자로_이루어지지_않은_리스트_테스트")
+    @Test
+    void invalidateNumbersCount() {
+        List<Integer> invalideNumbers = List.of(1, 2, 3, 4, 5);
+        assertThatThrownBy(() -> LottoNumberValidator.validateNumbersCount(invalideNumbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 6개이여야 합니다.");
     }
 }
