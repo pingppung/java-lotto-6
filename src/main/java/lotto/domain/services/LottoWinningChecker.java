510104bab7395f4f577d2a9f2cc6264ad3a810e0
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-10 02:47:02 +0900

    feat(LottoWinningChecker): 구매한 로또 번호와 당첨 번호 비교하는 클래스 생성

---
+++ src/main/java/lotto/domain/services/LottoWinningChecker.java
@@ -0,0 +1,26 @@
+package lotto.domain.services;
+
+import java.util.EnumMap;
+import java.util.List;
+import lotto.constants.LottoRank;
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
+
+public class LottoWinningChecker {
+    private final List<Lotto> purchasedLottos;
+    private final WinningLotto winningLotto;
+    private final EnumMap<LottoRank, Integer> rankCount;
+
+    public LottoWinningChecker(List<Lotto> purchasedLottos, WinningLotto winningLotto) {
+        this.purchasedLottos = purchasedLottos;
+        this.winningLotto = winningLotto;
+        this.rankCount = new EnumMap<>(LottoRank.class);
+        initializePrizeCount();
+    }
+
+    private void initializePrizeCount() {
+        for (LottoRank rank : LottoRank.values()) {
+            rankCount.put(rank, 0);
+        }
+    }
+}
