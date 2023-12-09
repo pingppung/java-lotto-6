b70bb9cd89dd773083bbcd0b4eb35915d7cec933
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-10 03:52:26 +0900

    feat(EarningRateCalculator): (당첨금액 / 구입금액) * 100 계산하여 총 수익률를 구하는 기능 구현

--- src/main/java/lotto/domain/services/EarningRateCalculator.java
+++ src/main/java/lotto/domain/services/EarningRateCalculator.java
@@ -1,14 +1,23 @@
 package lotto.domain.services;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.EnumMap;
 import lotto.constants.LottoRank;
 
 public class EarningRateCalculator {
-    private static BigDecimal calculateTotalPrizeAmount(EnumMap<LottoRank, Integer> ranks) {
-        return ranks.entrySet().stream().filter(entry -> entry.getValue() > 0)
+    private static final int MIN_WINNING_COUNT = 0;
+    private static final int ROUNDING_SCALE = 3;
+
+    private static BigDecimal calculateTotalRankAmount(EnumMap<LottoRank, Integer> ranks) {
+        return ranks.entrySet().stream().filter(entry -> entry.getValue() > MIN_WINNING_COUNT)
                 .mapToDouble(entry -> entry.getKey().getPrizeAmount())
                 .mapToObj(BigDecimal::valueOf)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
+
+    private static BigDecimal calculateEarningRate(BigDecimal totalRankAmount, int purchaseAmount) {
+        return totalRankAmount.divide(BigDecimal.valueOf(purchaseAmount), ROUNDING_SCALE, RoundingMode.DOWN);
+    }
+
 }
