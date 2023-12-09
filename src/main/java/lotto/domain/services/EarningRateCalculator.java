970e668cb63dd6cedecd371f8eebafe1a152b47e
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-10 03:58:22 +0900

    feat(EarningRateCalculator): 수익률을 계산 로직 및 소수점 둘째 자리에서 반올림 처리

--- src/main/java/lotto/domain/services/EarningRateCalculator.java
+++ src/main/java/lotto/domain/services/EarningRateCalculator.java
@@ -7,7 +7,14 @@ import lotto.constants.LottoRank;
 
 public class EarningRateCalculator {
     private static final int MIN_WINNING_COUNT = 0;
-    private static final int ROUNDING_SCALE = 3;
+    private static final int ROUNDING_PRECISION_ROUND = 1;
+    private static final int ROUNDING_PRECISION_TRUNCATE = 3;
+
+    public static BigDecimal calculate(EnumMap<LottoRank, Integer> prizes, int purchaseMoney) {
+        BigDecimal totalWinnings = calculateTotalRankAmount(prizes);
+        BigDecimal earningRate = calculateEarningRateInPercentage(totalWinnings, purchaseMoney);
+        return earningRate.setScale(ROUNDING_PRECISION_ROUND, RoundingMode.HALF_UP);
+    }
 
     private static BigDecimal calculateTotalRankAmount(EnumMap<LottoRank, Integer> ranks) {
         return ranks.entrySet().stream().filter(entry -> entry.getValue() > MIN_WINNING_COUNT)
@@ -17,7 +24,8 @@ public class EarningRateCalculator {
     }
 
     private static BigDecimal calculateEarningRate(BigDecimal totalRankAmount, int purchaseAmount) {
-        return totalRankAmount.divide(BigDecimal.valueOf(purchaseAmount), ROUNDING_SCALE, RoundingMode.DOWN);
+        return totalRankAmount.divide(BigDecimal.valueOf(purchaseAmount), ROUNDING_PRECISION_TRUNCATE,
+                RoundingMode.DOWN);
     }
 
     private static BigDecimal calculateEarningRateInPercentage(BigDecimal totalRankAmount, int purchaseAmount) {
