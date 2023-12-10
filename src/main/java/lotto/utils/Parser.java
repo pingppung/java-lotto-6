fff453aa36a5a127febc8f885bab877c33063f0f
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-10 23:05:40 +0900

    feat(Parser): 상금 천 단위마다 쉼표 표시 기능 구현

--- src/main/java/lotto/utils/Parser.java
+++ src/main/java/lotto/utils/Parser.java
@@ -1,10 +1,13 @@
 package lotto.utils;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
     private static final String SEPARATOR = ",";
+    private static final String MONEY_CURRENCY_PATTERN = "###,###";
 
     public static List<Integer> parseWinnginNumbers(String input) {
         return Arrays.stream(input.split(SEPARATOR))
@@ -16,5 +19,15 @@ public class Parser {
     private static Integer convertToInt(String number) {
         return InputValidator.validateNonNumeric(number);
     }
+
+    private static String formatCurrency(BigDecimal amount, String pattern) {
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
+        return decimalFormat.format(amount);
+    }
+
+    public static String formatMoneyCurrency(BigDecimal amount) {
+        return formatCurrency(amount, MONEY_CURRENCY_PATTERN);
+    }
+
 
 }
