a596215fbcb6ff0b71fa577c3234d29ca76a9b47
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-10 23:20:13 +0900

    test(ParserTest): 돈 표시할 때 천 단위마다 쉼표 포맷팅 테스트

--- src/test/java/lotto/utils/ParserTest.java
+++ src/test/java/lotto/utils/ParserTest.java
@@ -1,18 +1,28 @@
 package lotto.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ParserTest {
 
-    @DisplayName("입력값을_쉼표_기준으로_정확히_분리되는지_확인")
+    @DisplayName("입력값을 쉼표 기준으로 정확히 분리되는지 확인")
     @Test
     void validateParseWinnginNumbers() {
         String input = "1,2,3";
         List<Integer> expectedResult = List.of(1, 2, 3);
         assertThat(Parser.parseWinnginNumbers(input)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("금액을 천 단위로 포맷팅 확인")
+    @Test
+    void formatMoneyCurrency() {
+        BigDecimal amount = new BigDecimal("2000000000");
+        String formattedAmount = Parser.formatMoneyCurrency(amount);
+        assertEquals("2,000,000,000", formattedAmount);
     }
 }
