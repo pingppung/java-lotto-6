3d74df1a987da1466c80ba85f142aff044d5543e
Pingppung <57535999+pingppung@users.noreply.github.com>
2023-12-11 00:42:59 +0900

    feat(WinningStatisticsResult): 각 순위에 해당하는 당첨 횟수로 문자열을 형식화하여 반환하는 기능 구현

--- src/main/java/lotto/domain/WinningStatisticsResult.java
+++ src/main/java/lotto/domain/WinningStatisticsResult.java
@@ -1,13 +1,32 @@
 package lotto.domain;
 
 import java.math.BigDecimal;
+import java.util.EnumMap;
+import java.util.Map;
 import lotto.constants.LottoRank;
 import lotto.utils.Parser;
 
 public class WinningStatisticsResult {
     private static final String DEFAULT_RANK_PRINT_FORMAT = "%d개 일치 (%s원) - %d개";
     private static final String BONUS_RANK_PRINT_FORMAT = "%d개 일치, 보너스 볼 일치 (%s원) - %d개";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
 
+    public static String generateRankResults(EnumMap<LottoRank, Integer> ranks) {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<LottoRank, Integer> entry : ranks.entrySet()) {
+            LottoRank rank = entry.getKey();
+            int winningCount = entry.getValue();
+
+            String formattedResult = formatRankResult(rank, winningCount);
+            sb.append(formattedResult).append(LINE_SEPARATOR);
+        }
+        return sb.toString();
+    }
+
+    private static String formatRankResult(LottoRank rank, int winningCount) {
+        String resultSentence = selectRankSentence(rank);
+        return buildFormattedRankResult(rank, winningCount, resultSentence);
+    }
 
     private static String selectRankSentence(LottoRank rank) {
         String resultSentence = DEFAULT_RANK_PRINT_FORMAT;
